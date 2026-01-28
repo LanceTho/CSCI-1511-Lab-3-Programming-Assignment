b83b8128b9134f9281bef3ac7d5073cb6555a0d4
Lance Thongsavanh <lancer8106@gmail.com>
2026-01-28 17:27:19 -0500

    Imported list from Lab3_LanceTho_replace.py and prints the list

--- Lab3_LanceTho_remove.py
+++ Lab3_LanceTho_remove.py
@@ -1,7 +1,13 @@
 """
-The program's name,
-Your name (the author),
-The purpose of the program,
-Any info about starter code (If used, where it came from, link, etc.), and the
-Date.
+Lab3_LanceTho_remove.py
+Lance Thongsavanh
+Import the list from your Lab3_username_replace.py file.
+You decided you don't have binoculars, so remove that object from the list.
+Print the resulting list.
+Print an f-string stating how many total items will be brought to the camping trip using the len() function
+1/28/2026
 """
+
+import Lab3_LanceTho_replace
+
+print(f"{Lab3_LanceTho_replace.Lab3_LanceTho_add.Lab3_LanceTho_list.supplies}")
