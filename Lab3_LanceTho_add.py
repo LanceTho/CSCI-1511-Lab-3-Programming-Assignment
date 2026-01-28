6edd9aee7a11001b6cd610afd648fe34392ecdda
Lance Thongsavanh <lancer8106@gmail.com>
2026-01-28 16:57:34 -0500

    Imported list from Lab3_LanceTho_list.py and appended a new element and prints the full list

--- Lab3_LanceTho_add.py
+++ Lab3_LanceTho_add.py
@@ -1,7 +1,12 @@
 """
-The program's name,
-Your name (the author),
-The purpose of the program,
-Any info about starter code (If used, where it came from, link, etc.), and the
-Date.
+Lab3_LanceTho_add.py
+Lance Thongsavanh
+Import the list from your Lab3_username_list.py file. Do not copy and paste the list definition.
+Append 5 more items to the imported list.
+Print the resulting list reversed alphabetically.
+1/28/2026
 """
+import Lab3_LanceTho_list
+
+Lab3_LanceTho_list.supplies.append("Lantern")
+print(f"{Lab3_LanceTho_list.supplies}")
