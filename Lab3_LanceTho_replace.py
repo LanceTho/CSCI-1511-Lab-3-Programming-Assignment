d46fcab7c91edbb10e7e2956b1f95c118d85fa78
Lance Thongsavanh <lancer8106@gmail.com>
2026-01-28 17:08:53 -0500

    Imported list from Lab3_LanceTho_add.py and prints out the list

--- Lab3_LanceTho_replace.py
+++ Lab3_LanceTho_replace.py
@@ -1,7 +1,15 @@
 """
-The program's name,
-Your name (the author),
-The purpose of the program,
-Any info about starter code (If used, where it came from, link, etc.), and the
-Date.
+Lab3_LanceTho_replace.py
+Lance Thongsavanh
+Import the list from your Lab3_username_add.py file.
+Replace one of the string items from the list with "binoculars", but don't replace the first or the last element in the list.
+    Using Slice Notation,
+    print the list from the start of the list to the binoculars item(not including the binoculars),
+    the binoculars,
+    and the list from the first item after the binoculars to the end of the list
+1/28/2026
 """
+
+import Lab3_LanceTho_add
+
+print(f"{Lab3_LanceTho_add.Lab3_LanceTho_list.supplies}")
